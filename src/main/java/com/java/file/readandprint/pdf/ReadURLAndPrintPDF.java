432fe019df8f03233ac4179c39547ac5cfb8c35e
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-07-30 17:14:41 +0530

    code changes

--- src/main/java/com/java/file/readandprint/pdf/ReadURLAndPrintPDF.java
+++ src/main/java/com/java/file/readandprint/pdf/ReadURLAndPrintPDF.java
@@ -1,12 +1,15 @@
 package com.java.file.readandprint.pdf;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
@@ -16,32 +19,31 @@ import lombok.extern.slf4j.Slf4j;
 public class ReadURLAndPrintPDF {
 	
 	public static void main(String[] args) {
-		File file = null;
+		File targetFile = null;
 		PDDocument document = null;
 		try {
-			StringBuilder content = new StringBuilder();
 			URL url = new URL("https://drive.google.com/open?id=0Bwz1Xin_KMzya1RtN3E0b2plam02YXhPLXlsUlhYUkRTdUtV");
 			InputStream inputStream = url.openStream();
 			String fileName = null;
 			fileName = FilenameUtils.getName(url.getPath());
 //			fileName = Paths.get(new URI(url.getFile()).getPath()).getFileName().toString();
-			file = new File(System.getProperty("user.dir") +"/src/main/resources/upload_files/"+ fileName +".pdf");
-			FileUtils.copyInputStreamToFile(inputStream, file);
-//			// wrap the urlconnection in a bufferedreader
-//		      BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-//
-//		      String line;
-//
-//		      // read from the urlconnection via the bufferedreader
-//		      while ((line = bufferedReader.readLine()) != null)
-//		      {
-//		        content.append(line + "\n");
-//		      }
-//		      bufferedReader.close();
-		      
-//			if (file != null) {
-//				document = PDDocument.load(file);
-//				if (!document.isEncrypted()) {
+			
+			byte[] buffer = new byte[inputStream.available()];
+		    inputStream.read(buffer);
+		 
+		    targetFile = new File(System.getProperty("user.dir") + "/src/main/resources/upload_files/" + fileName + "_"
+					+ ZonedDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH_mm_ss_SSS")) + ".pdf");
+		    log.info("Reading PDF : {}", targetFile.getName());
+		    OutputStream outStream = new FileOutputStream(targetFile);
+		    outStream.write(buffer);
+		    if (outStream != null) {
+		    	outStream.close();
+		    }
+		    
+//			if (targetFile != null) {
+//				log.info("Reading PDF file: {}", targetFile.getName());
+//				document = PDDocument.load(targetFile, "file_password");
+//				if (document.isEncrypted()) {
 //					PDFTextStripper stripper = new PDFTextStripper();
 //					String text = stripper.getText(document);
 //					String[] lines = text.split("\\r?\\n");
@@ -49,9 +51,10 @@ public class ReadURLAndPrintPDF {
 //						System.out.println(line);
 //					}
 //				} else {
-//					log.error("Encrypted Document: {}", file.getName());
+//					log.error("Encrypted Document: {}", targetFile.getName());
 //				}
 //			}
+			
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 		} catch (IOException e) {
