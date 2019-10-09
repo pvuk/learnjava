76b6d89f16fc3fe53d74f51ec36cbaa7ff681b90
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-10-09 17:49:48 +0530

    initial commit

---
+++ src/main/java/com/java/escapesequence/CharSequence.java
@@ -0,0 +1,18 @@
+package com.java.escapesequence;
+
+/**
+ * 
+ * @author udaykiranp
+ * 
+ * </br>
+ * 
+ *	<img src="{@docRoot}/../resources/images/java-basic-ASCII Table.png" />
+ */
+//<img src="../../../../resources/images/java-basic-ASCII Table.png" />
+public class CharSequence {
+	public static void main(String[] args) {
+		System.out.println((char) 33);
+		System.out.println((char)0x2B);//hexa decimal start with 0x
+		System.out.println((char)056);//octa decimal start with 0
+	}
+}
