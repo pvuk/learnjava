6c935baf3e2689467542202138093c3af733bb1c
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-10-11 16:12:08 +0530

    use two tags and other one is in reference

--- src/main/java/com/java/escapesequence/CharSequence.java
+++ src/main/java/com/java/escapesequence/CharSequence.java
@@ -5,10 +5,10 @@ package com.java.escapesequence;
  * @author udaykiranp
  * 
  * </br>
- * 
- *	<img src="{@docRoot}/../resources/images/java-basic-ASCII Table.png" />
+ *
+ * 	<img src="{@docRoot}/../resources/images/java-basic-ASCII Table.png" />
+ * 	@see <a href="../../../../resources/images/java-basic-ASCII Table.png">java-basic-ASCII Table.png</a>
  */
-//<img src="../../../../resources/images/java-basic-ASCII Table.png" />
 public class CharSequence {
 	public static void main(String[] args) {
 		System.out.println((char) 33);
