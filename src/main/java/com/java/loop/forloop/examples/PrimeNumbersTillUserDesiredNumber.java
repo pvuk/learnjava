379618c8feee2b7d352112910fb5725cf1dae059
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-10-18 18:48:44 +0530

    java doc

--- src/main/java/com/java/loop/forloop/examples/PrimeNumbersTillUserDesiredNumber.java
+++ src/main/java/com/java/loop/forloop/examples/PrimeNumbersTillUserDesiredNumber.java
@@ -2,6 +2,27 @@ package com.java.loop.forloop.examples;
 
 import java.util.Scanner;
 
+/**
+ * <h1><a href="https://www.guru99.com/prime-number-program-java.html">Program Logic:</a></h1>
+ * <ul>
+ * <li>The main method contains a loop to check prime numbers one by one.</li>
+ * <li>The main method calls the method CheckPrime to determine whether a number
+ * is prime</li>
+ * <li>We need to divide an input number, say 17 from values 2 to 17 and check
+ * the remainder. If the remainder is 0 number is not prime.</li>
+ * <li>No number is divisible by more than half of itself. So, we need to loop
+ * through just numberToCheck/2. If the input is 17, half is 8.5, and the loop
+ * will iterate through values 2 to 8</li>
+ * <li>If numberToCheck is entirely divisible by another number, we return
+ * false, and loop is broken.</li>
+ * <li>If numberToCheckis prime, we return true.</li>
+ * <li>In the main method, check isPrime is TRUE and add to primeNumbersFound
+ * String</li>
+ * </ul>
+ * 
+ * @author venkataudaykiranp
+ *
+ */
 public class PrimeNumbersTillUserDesiredNumber {
 	public static void main(String[] args) {
 		Scanner scanner = null;
@@ -14,7 +35,7 @@ public class PrimeNumbersTillUserDesiredNumber {
 				// prime number check
 				boolean isPrime = isPrime(i);
 				if (isPrime) {
-					System.out.print(i +" ");
+					System.out.print(i + " ");
 				}
 			}
 		} catch (Exception e) {
@@ -30,7 +51,8 @@ public class PrimeNumbersTillUserDesiredNumber {
 		int remainder;
 		for (int j = 2; j <= numToCheck / 2; j++) {
 			remainder = numToCheck % j;
-			//if remainder is 0 than numToCheck is not prime and break loop. Else continue loop
+			// if remainder is 0 than numToCheck is not prime and break loop. Else continue
+			// loop
 			if (remainder == 0) {
 				return false;
 			}
