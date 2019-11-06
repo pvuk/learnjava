986bfefa674b187d8794737b224c6bb9887b44aa
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2019-11-06 18:37:23 +0530

    code twist

--- src/main/java/com/java/interview/productbased/adp/InheritanceCount.java
+++ src/main/java/com/java/interview/productbased/adp/InheritanceCount.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class InheritanceCount implements Alphabets {
 
+//	public int i = 4;//this variable value not consider
+	
 	public static void main(String[] args) {
 		Alphabets a = new InheritanceCount();
 		Number b = new InheritanceCount();
