3e44d46e54dad6e31b42efd5e386b591d782fefb
Priyanka <Priyanka@LAPTOP-OJ9GMP1L>
2019-11-11 16:14:03 +0530

    Linkedin Assessment

---
+++ src/main/java/com/java/interview/linkedin/assessment/IntArraySize.java
@@ -0,0 +1,12 @@
+package com.java.interview.linkedin.assessment;
+
+//uncomment the code to execute
+//It will not compile because of line 4.
+class IntArraySize {
+	public static void main(String[] args) {
+		int array[] = { 1, 2, 3, 4 };
+//     for (int i=0; i<array.size(); i++){//line4
+//       System.out.print(array[i]);
+//     }
+	}
+}
---
+++ src/main/java/com/java/interview/linkedin/assessment/SubString.java
@@ -0,0 +1,8 @@
+package com.java.interview.linkedin.assessment;
+
+public class SubString {
+	public static void main(String[] args) {
+		String subString = "strawberries";
+		System.out.println(subString.substring(2, 5));
+	}
+}
