801af0ef146c3dc95f035ab533887e1886c5c510
P V UdayKiran <pvuk1988@gmail.com>
2019-12-06 12:13:37 +0530

    In inheritance, subclass acquires super class properties. An important point to note is, when subclass object is created, a separate object of super class object will not be created. Only a subclass object object is created that has super class variables.

--- src/main/java/com/java/inheritance/examples/AppleTest.java
+++ src/main/java/com/java/inheritance/examples/AppleTest.java
@@ -1,10 +1,7 @@
 package com.java.inheritance.examples;
 
 /**
- * 
- * @author venkataudaykiranp
- * 
- * <p>
+* <p>
  * <h1>Java Object Creation of Inherited Class</h1>
 In inheritance, subclass acquires super class properties. An important point to note is, when subclass object is created, a separate object of super class object will not be created. Only a subclass object object is created that has super class variables.
 <br>
@@ -14,19 +11,25 @@ This situation is different from a normal assumption that a constructor call mea
  * <code>
  * <b>Output:</b>
 
-super class constructor <br>
-super class object hashcode :366712642<br>
-Apple<br>
-sub class constructor <br>
-sub class object hashcode :366712642<br>
-366712642   366712642<br>
-Apple  Apple<br>
-</code>
-<br>
-As we can see that both super class(Fruit) object hashcode and subclass(Apple) object hashcode are same, so only one object is created. This object is of class Apple(subclass) as when we try to print name of class which object is created, it is printing Apple which is subclass.
+	super class constructor <br>
+	super class object hashcode :366712642<br>
+	Apple<br>
+	sub class constructor <br>
+	sub class object hashcode :366712642<br>
+	366712642   366712642<br>
+	Apple  Apple<br>
+	</code>
+	<br>
+	As we can see that both super class(Fruit) object hashcode and subclass(Apple) object hashcode are same, so only one object is created. This object is of class Apple(subclass) as when we try to print name of class which object is created, it is printing Apple which is subclass.
  *</p>
+ * 
+ * @author venkataudaykiranp
+ *
+ * @Date Fri 06-Dec-2019 11:23
+ * <p>version annotation added. if any changes it automatically updated the version. review it.</p>
+ * @version: %I%
  */
-public class TestApple {
+public class AppleTest {
 	
 	public static void main(String[] args) 
     { 
