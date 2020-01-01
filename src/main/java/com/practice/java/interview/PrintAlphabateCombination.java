cb43fa0579467b77fe5d91c86e6a36e0fb72710c
P V UdayKiran <pvuk1988@gmail.com>
2020-01-01 22:43:23 +0530

    Print pattern ABC, BCD, CDE, DEF, EFG, FGH, GHI, HIJ, IJK, JKL, KLM, LMN, MNO, NOP, OPQ, PQR, QRS, RST, STU, TUV, UVW, VWX, WXY, XYZ using recursive example

--- src/main/java/com/practice/java/interview/PrintAlphabateCombination.java
+++ src/main/java/com/practice/java/interview/PrintAlphabateCombination.java
@@ -5,8 +5,35 @@ package com.practice.java.interview;
  * Write a program to print "ABC", "BCD", "CDE", "DEF", ....., "WXY", "XYZ"
  * 
  * @author Priyanka
+ * @author P V UdayKiran
  *
  */
 public class PrintAlphabateCombination {
+	public static void main(String[] args) {
+		exampleOne();
+	}
 
+	private static void exampleOne() {
+		StringBuilder sb = new StringBuilder();
+		// char 65 = "A", char 90 = "Z"
+		for (int i = 64; i < 90; i++) {
+			// char 87 = X, i increment upto 3 i.e 90 = Z
+			if (i < 88) {
+				i = printAlphabet(i, 1, sb);// using recursive method
+				// char 88 = X, avoid comma append
+				if (i < 87) {
+					sb.append(", ");
+				}
+			}
+		}
+		System.out.println(sb.toString());
+	}
+
+	private static int printAlphabet(int i, int combinationCount, StringBuilder sb) {
+		if (combinationCount > 3) {
+			return i;
+		}
+		sb.append((char) (i + combinationCount));
+		return printAlphabet(i, combinationCount + 1, sb);
+	}
 }
