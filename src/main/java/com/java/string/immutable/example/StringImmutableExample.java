fcbc6c5a6d8f3da018ddff51df494cbf7f53c851
venkata udaykiran p <venkataudaykiranp@h-PUday.ffe.foxeg.com>
2020-01-24 19:23:30 +0530

    equals and == comparison examples

--- src/main/java/com/java/string/immutable/example/StringImmutableExample.java
+++ src/main/java/com/java/string/immutable/example/StringImmutableExample.java
@@ -23,7 +23,11 @@ public class StringImmutableExample {
 		log.info("Trying to change s1 value from: {} to .toUpperCase(): {} without assigning to s1 and s1 hashcode {}", s1, s1.toUpperCase(), s1.hashCode());
 		
 		log.info("Trying to change s1 value from: {} to .toUpperCase(): {} by assigning, s1 hashcode: {}", s1, s1 = s1.toUpperCase(), s1.hashCode());
+		
+		
 		System.out.println("===================================================================");
+		
+		
 		String firstName = "Mahendra Singh";
 		log.info("firstName: {}, firstName hashcode: {}", firstName, firstName.hashCode());
 		
@@ -36,6 +40,48 @@ public class StringImmutableExample {
 				firstName, lastName, firstName = firstName.concat(lastName), firstName.hashCode());
 
 		log.info("firstName: {}, firstName hashcode: {}, lastName: {}, lastName hashcode: {}", firstName, firstName.hashCode(), lastName, lastName.hashCode());
+		
+		
+		System.out.println("===================================================================");
+		
+		
+		String a = "test";
+		int aHashCode = a.hashCode();
+		log.info("a value: {}, a hashCode: {}", a, aHashCode);
+		String b = "TEST";
+		log.info("b value: {}, b hashCode: {}", b, b.hashCode());
+		a = b;
+		log.warn("b value \"TEST\" is to a. a value: {}, a hashCode: {}, b value: {}, b hashCode: {}", a, a.hashCode(), b, b.hashCode());
+		log.warn("Previous a value \"test \" is initialized in c String value");
+		String c = "test";
+		int cHashCode = c.hashCode();
+		log.info("c value: {}, c hashCode: {}", c, c.hashCode());
+		if(aHashCode == cHashCode) {
+			log.error("\"test\" value is taken from String cache pool");
+		}
+		
+		System.out.println("===================================================================");
+		
+		
+		String x = "Hello World!";
+		String y = new String("Hello World!");
+		String z = new String("Hello World!");
+		
+		String A = new String(x);
+		String B = new String(y);
+		
+		String p = "Hello World!";
+		String q = x;
+		String r = p;
+		
+		log.info("\n x value: {}, x hashCode: {},\n y value: {}, y hashCode: {},\n z value: {}, z hashCode: {},\n A value: {}, A hashCode: {},\n B value: {}, B hashCode: {},"
+				+ "\n p value: {}, p hashCode: {},\n q value: {}, q hashCode: {},\n r value: {}, r hashCode: {}",
+				x, x.hashCode(), y, y.hashCode(), z, z.hashCode(), A, A.hashCode(), B, B.hashCode(), p, p.hashCode(), q, q.hashCode(), r, r.hashCode());
+		log.info("[true / false condition] x equals y: {}, y equals z: {}, x equals A: {}, A equals B: {}, x equals p: {}, q equals r: {}",
+				x.equals(y), y.equals(z), x.equals(A), A.equals(B), x.equals(p), q.equals(r));
+		log.info("[true / false condition] x == y: {}, y == z: {}, x == A: {}, A == B: {}, x == p: {}, q == r: {}",
+				x == y, y == z, x == A, A == B, x == p, q == r);
+		
 		System.out.println("===================================================================");
 	}
 }
