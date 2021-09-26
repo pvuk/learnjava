68cc7c70030514f966c552b1105d112c8b594494
P. V. UdayKiran <pvuk1988@gmail.com>
2021-09-26 18:24:25 +0530

    file access changes

--- src/main/java/com/java/lambda/sort/SortListUsingLambda.java
+++ src/main/java/com/java/lambda/sort/SortListUsingLambda.java
@@ -7,7 +7,31 @@ import java.util.stream.Collectors;
 
 import com.java.model.JsonCodeDocument;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SortListUsingLambda {
+	
+	public static void main(String[] args) {
+		String pathname = "/src/main/resources/json/put/put-card_status_code.json";
+//		try {
+//			File file = new File(System.getProperty("user.dir") + pathname);
+//			InputStream iStream = null;
+//			List<JsonCodeDocument> alJCD = new ArrayList<JsonCodeDocument>();
+//			if(file.exists()) {
+//				JSONParser jsonParser = new JSONParser(new FileReader(file));
+//				ArrayList<Object> arrayList = jsonParser.parseArray();
+//				alJCD = new ObjectMapper().convertValue(arrayList, new TypeReference<List<JsonCodeDocument>>() {});
+//				if(alJCD != null) {
+//					log.info("Json Array Data: ");
+//					System.out.println(alJCD);
+//				}
+//			} else {
+//				log.error("File not found in path: {}", file);
+//			}
+//		}
+	}
+	
 	public static List<JsonCodeDocument> usingComparator(List<JsonCodeDocument> list) {
 		Comparator<JsonCodeDocument> ascSort = (JsonCodeDocument jcd1, JsonCodeDocument jcd2) -> {
 			return jcd1.getCode().compareTo(jcd2.getCode());
