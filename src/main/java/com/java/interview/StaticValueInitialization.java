667c7bedcf970fbfd63199cde017182b033a8af5
P. V. UdayKiran <pvuk1988@gmail.com>
2021-09-26 18:12:12 +0530

    Initial commit, package update

--- src/main/java/com/java/interview/StaticValueInitialization.java
+++ src/main/java/com/java/interview/StaticValueInitialization.java
@@ -1,4 +1,4 @@
-package com.java.interview.nextlevel;
+package com.java.interview;
 
 /**
  * @author P.V. UdayKiran
--- src/main/java/com/java/interview/nextlevel/PrintAlphabateCombination.java
+++ src/main/java/com/java/interview/nextlevel/PrintAlphabateCombination.java
@@ -1,4 +1,4 @@
-package com.java.interview;
+package com.java.interview.nextlevel;
 
 /**
  * 
