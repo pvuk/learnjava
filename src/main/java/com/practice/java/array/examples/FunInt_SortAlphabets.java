33de519196f315c5cb183ee34b7a626c3037d2d9
Venkata.Pulipati <Venkata.Pulipati@LP-HYD-0X134610.IN.COFORGETECH.COM>
2025-07-28 15:28:45 +0530

    sts code templates file initial commit, sorting using different algorithms in java

--- src/main/java/com/practice/java/array/examples/FunInt_SortAlphabets.java
+++ src/main/java/com/practice/java/array/examples/FunInt_SortAlphabets.java
@@ -3,7 +3,7 @@ package com.practice.java.array.examples;
 import java.util.List;
 
 @FunctionalInterface
-public interface SortAlphabets {
+public interface FunInt_SortAlphabets {
 	 
 	abstract List<String> sort(List<String> list, SortOrder direction);
 }
