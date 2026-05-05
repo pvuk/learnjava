985ef807a06117910be87b548d3737a1a936c4e1
Venkata Uday Kiran Pulipati <Venkata.Pulipati@coforge.com>
2026-05-05 17:02:54 +0530

    Reverse string examples using Loop, Streams

--- src/main/java/com/java/interview/coforge/ReverseStringWords.java
+++ src/main/java/com/java/interview/coforge/ReverseStringWords.java
@@ -3,19 +3,30 @@ package com.java.interview.coforge;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ReverseStringWords {
 	public static void main(String[] args) {
-		//Problem Statement 1
-				String str = "Hello My Name is Test";
-				//Convert it as "Test is Name My Hello"
-//				Arrays.asList(str.split(" ")).stream()
-				List<String> asList = Arrays.asList(str.split(" "));
-				Collections.reverse(asList);
-				System.out.println(asList);
-				String join = String.join(" ", asList);
-				System.out.println(join);//convert list to string
+		String str = "Hello My Name is Test";
+		List<String> asList = Arrays.asList(str.split(" "));
+		Collections.reverse(asList);
+		System.out.println("Reverse using Collections: "+ asList);
+		
+		exampleUsingLoop(str);
+		
+		exampleUsingStreams(str);
+		
+	}
+
+	private static void exampleUsingStreams(String str) {
+		String reverse = Arrays.asList(str.split(" ")).stream().reduce((a, b) -> b +" "+ a).get();
+		System.out.println("\nReverse Sentence Using Streams: "+ reverse);
+	}
+
+	private static void exampleUsingLoop(String str) {
+		String[] words = str.split(" ");
+		System.out.println("Reverse Using Loop: ");
+		for(int i = words.length - 1 ; i >= 0; i--) {
+			System.out.print(" "+ words[i]);
+		}
 	}
 }
